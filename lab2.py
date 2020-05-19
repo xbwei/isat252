08939f67d3831672aed4f2b21b05cc23b08e1280
EC2 Default User <ec2-user@ip-172-31-101-201.ec2.internal>
2020-05-19 16:01:06 +0000

    add my second py files

---
+++ lab2.py
@@ -0,0 +1,45 @@
+"""
+Lab 2
+"""
+
+# 4.1
+my_name = "Tom"
+print(my_name.upper())
+
+# 4.2
+
+my_id = 123
+print(my_id)
+
+
+#4.3
+my_id=your_id=123
+print(my_id)
+print(your_id)
+
+#4.4
+
+my_id_str = "123"
+print(my_id_str)
+
+#4.5
+
+#print(my_name+my_id)
+
+#4.6
+
+print(my_name+my_id_str)
+
+#4.7
+
+print(my_name*3)
+
+#4.8
+
+print('hello world. This is my first python string.'.split('.'))
+
+#4.9
+
+#message = 'Tom's ID is 123'
+
+#print(message)
