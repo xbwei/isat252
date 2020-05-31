4f0a220b9e9759c20ec26e253c40974feb04828a
EC2 Default User <ec2-user@ip-172-31-101-201.ec2.internal>
2020-05-31 23:25:10 +0000

    day7

---
+++ lab7.py
@@ -0,0 +1,33 @@
+"""
+lab7
+"""
+#3.1
+
+# i = 0 
+
+# while i <6:
+    
+#     if i !=3:
+#         print(i)
+    
+#     i = i +1
+
+#3.5
+
+# i = 4
+# result = 1
+
+# while i <=8:
+#     # print(i)
+#     result = result *i
+#     i= i+1
+# print(result)
+
+#3.6
+
+num_list=[12,32,43,35]
+
+while num_list:
+  num_list.remove(num_list[0])
+
+print(num_list)
---
+++ lec7.py
@@ -0,0 +1,33 @@
+"""
+lec7
+"""
+
+i = 5
+
+while i >=0 :
+    
+    try:
+        print(1/(i-3))
+    
+    except:
+        continue
+    i = i -1
+
+
+# for word in 'hello world'.split():
+#     print (word)
+    
+#     for str_item in word:
+        
+#         if str_item =='l':
+#             break
+#         print(str_item)
+
+# try:
+#     print(1/0)
+    
+# except ZeroDivisionError:
+#     print('division by zero')
+
+    
+    
